87f531de8248d55defa905c1fe30648663bb55e2
Antonin06 <avon.antonin@gmai.com>
2021-06-16 14:04:15 +0200

    working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Cities from './cities.json';
+import {useState} from "react";
+import { Typeahead } from 'react-bootstrap-typeahead';
 
 function App() {
+    const [singleSelections, setSingleSelections] = useState([]);
+    console.log(singleSelections, "yolo ça marche");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Typeahead
+            clearButton
+            id="basic-typeahead-single"
+            labelKey="name"
+            maxResults={6}
+            paginationText="Voir plus"
+            onChange={setSingleSelections}
+            options={Cities}
+            placeholder="Votre Ville..."
+            selected={singleSelections}
+            renderMenuItemChildren={(city) => (
+                <div>
+                    <small>{city.name}</small>
+                </div>
+            )}
+        />
     </div>
   );
 }
